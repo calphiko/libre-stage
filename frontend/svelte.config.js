37b75fa95f7711a1360b5f24355d6f569d225932
dekoppca <c.koppen@pmdtec.com>
2026-03-03 16:14:20 +0100

    fix: switch adapter to @sveltejs/adapter-node and add vitePreprocess

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-
-	kit: {
-		adapter: adapter()
-	}
+  extensions: ['.svelte'],
+  preprocess: [vitePreprocess()],
+  kit: {
+    adapter: adapter()
+  }
 };
+
 export default config;
