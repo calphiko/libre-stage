16611aa42a10bfa1770fd4dabeac949e8b35ea1e
calle <cp@koppen.me>
2026-04-12 14:09:43 +0200

    feat: apply global appearance settings for navigation and tab bars

--- ios/LibreStage/App/LibreStageApp.swift
+++ ios/LibreStage/App/LibreStageApp.swift
@@ -3,17 +3,44 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import SwiftUI
+#if canImport(UIKit)
+import UIKit
+#endif
 import UserNotifications
 
 @main
 struct LibreStageApp: App {
     @State private var authManager = AuthManager()
 
+    init() {
+        applyGlobalAppearance()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentRoot()
                 .environment(authManager)
         }
+    }
+
+    private func applyGlobalAppearance() {
+        #if canImport(UIKit)
+        let navAppearance = UINavigationBarAppearance()
+        navAppearance.configureWithTransparentBackground()
+        navAppearance.backgroundColor = UIColor.systemBackground.withAlphaComponent(0.86)
+        navAppearance.titleTextAttributes = [.foregroundColor: UIColor.label]
+        navAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+
+        UINavigationBar.appearance().standardAppearance = navAppearance
+        UINavigationBar.appearance().compactAppearance = navAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
+
+        let tabAppearance = UITabBarAppearance()
+        tabAppearance.configureWithTransparentBackground()
+        tabAppearance.backgroundColor = UIColor.systemBackground.withAlphaComponent(0.92)
+        UITabBar.appearance().standardAppearance = tabAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabAppearance
+        #endif
     }
 }
 
