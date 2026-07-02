08e68c470bdaa642d0b6780078550429f135b91c
dekoppca <c.koppen@pmdtec.com>
2026-07-02 09:30:48 +0200

    fix: modified close action of modalStates.

--- frontend/src/lib/modalState.js
+++ frontend/src/lib/modalState.js
@@ -5,6 +5,7 @@ import { writable, get } from 'svelte/store';
 const { subscribe, set } = writable(null);
 
 let _responseCallback = null;
+let _onCloseCallback = null;
 
 export const modalState = {
   subscribe,
@@ -14,10 +15,12 @@ export const modalState = {
    * @param {object} opts
    * @param {import('svelte').Component} opts.component - Svelte component to render
    * @param {object} [opts.props] - Props to pass to the component
-   * @param {function} [opts.response] - Callback when modal responds
+   * @param {function} [opts.response] - Callback when modal responds (with value)
+   * @param {function} [opts.onClose] - Callback always called when modal closes
    */
-  trigger({ component, props, response, meta, title }) {
+  trigger({ component, props, response, meta, title, onClose }) {
     _responseCallback = response || null;
+    _onCloseCallback = onClose || null;
     set({
       component,
       props: { ...(props || {}), meta: meta || {}, title: title || '' }
@@ -31,7 +34,11 @@ export const modalState = {
     if (_responseCallback && responseValue !== undefined) {
       _responseCallback(responseValue);
     }
+    if (_onCloseCallback) {
+      _onCloseCallback();
+    }
     _responseCallback = null;
+    _onCloseCallback = null;
     set(null);
   },
 
